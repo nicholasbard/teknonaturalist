5c56a44a6b051cbf329062d69dfab4ea933e2ac8
nicholasbard <134089808+nicholasbard@users.noreply.github.com>
2024-12-10 18:06:50 -0800

    Update Fungal.ITS.classifier.sh

--- Fungal.ITS.classifier.sh
+++ Fungal.ITS.classifier.sh
@@ -26,14 +26,14 @@ vsearch --fastx_filter data/finalcombined/${SAMPLE}.5_8S.full.partial.all --fast
 
 # search Genus ITS1/ITS2/5.8S sequences.
 ###### USER: Replace {SAMPLE} with name or other identier, e.g., ermanii.
-python3.9 dnabarcoder/dnabarcoder.py search -i data/finalcombined/${SAMPLE}.ITS1.full.partial.all -r dnabarcoder/ITS.refs/unite2024/unite2024ITS1.unique.phylum.fasta -ml 50 -p ../data/finalcombined/${SAMPLE}
-python3.9 dnabarcoder/dnabarcoder.py search -i data/finalcombined/${SAMPLE}.ITS2.full.partial.all -r dnabarcoder/ITS.refs/unite2024/unite2024ITS2.unique.phylum.fasta -ml 50 -p ../data/finalcombined/${SAMPLE}
-python3.9 dnabarcoder/dnabarcoder.py search -i data/finalcombined/${SAMPLE}.5_8S.100.full.partial.all -r dnabarcoder/ITS.refs/unite2024/5_8s.unique.class.fasta -ml 50 -p ../data/finalcombined/${SAMPLE}
+python3.12 dnabarcoder/dnabarcoder.py search -i data/finalcombined/${SAMPLE}.ITS1.full.partial.all -r dnabarcoder/ITS.refs/unite2024/unite2024ITS1.unique.phylum.fasta -ml 50 -p ../data/finalcombined/${SAMPLE}
+python3.12 dnabarcoder/dnabarcoder.py search -i data/finalcombined/${SAMPLE}.ITS2.full.partial.all -r dnabarcoder/ITS.refs/unite2024/unite2024ITS2.unique.phylum.fasta -ml 50 -p ../data/finalcombined/${SAMPLE}
+python3.12 dnabarcoder/dnabarcoder.py search -i data/finalcombined/${SAMPLE}.5_8S.100.full.partial.all -r dnabarcoder/ITS.refs/unite2024/5_8s.unique.class.fasta -ml 50 -p ../data/finalcombined/${SAMPLE}
 
 # conduct local best match search with classify
-python3.9 dnabarcoder/dnabarcoder.py classify -i data/finalcombined/${SAMPLE}.unite2024ITS1.unique.phylum_BLAST.bestmatch -c dnabarcoder/ITS.refs/unite2024/unite2024ITS1.unique.phylum.classification -cutoffs dnabarcoder/ITS.refs/unite2024/unite2024ITS1.unique.cutoffs.best.json -p ../data/finalcombined/${SAMPLE}.ITS1
-python3.9 dnabarcoder/dnabarcoder.py classify -i data/finalcombined/${SAMPLE}.unite2024ITS2.unique.phylum_BLAST.bestmatch -c dnabarcoder/ITS.refs/unite2024/unite2024ITS2.unique.phylum.classification -cutoffs dnabarcoder/ITS.refs/unite2024/unite2024ITS2.unique.cutoffs.best.json -p ../data/finalcombined/${SAMPLE}.ITS2
-python3.9 dnabarcoder/dnabarcoder.py classify -i data/finalcombined/${SAMPLE}.5_8s.unique.class_BLAST.bestmatch -c dnabarcoder/ITS.refs/unite2024/5_8s.unique.class.classification -cutoffs dnabarcoder/ITS.refs/unite2024/unite20245_8s.cutoffs.json -p ../data/finalcombined/${SAMPLE}.5_8S
+python3.12 dnabarcoder/dnabarcoder.py classify -i data/finalcombined/${SAMPLE}.unite2024ITS1.unique.phylum_BLAST.bestmatch -c dnabarcoder/ITS.refs/unite2024/unite2024ITS1.unique.phylum.classification -cutoffs dnabarcoder/ITS.refs/unite2024/unite2024ITS1.unique.cutoffs.best.json -p ../data/finalcombined/${SAMPLE}.ITS1
+python3.12 dnabarcoder/dnabarcoder.py classify -i data/finalcombined/${SAMPLE}.unite2024ITS2.unique.phylum_BLAST.bestmatch -c dnabarcoder/ITS.refs/unite2024/unite2024ITS2.unique.phylum.classification -cutoffs dnabarcoder/ITS.refs/unite2024/unite2024ITS2.unique.cutoffs.best.json -p ../data/finalcombined/${SAMPLE}.ITS2
+python3.12 dnabarcoder/dnabarcoder.py classify -i data/finalcombined/${SAMPLE}.5_8s.unique.class_BLAST.bestmatch -c dnabarcoder/ITS.refs/unite2024/5_8s.unique.class.classification -cutoffs dnabarcoder/ITS.refs/unite2024/unite20245_8s.cutoffs.json -p ../data/finalcombined/${SAMPLE}.5_8S
 
 # format for R
 sed 's/\t/,/;s/\t//;s/\t/,/;s/\t/,/;s/\t/,/;s/\t/,/;s/\t/,/;s/\t/,/;s/\t/,/;s/\t/,/;s/.__//;s/;.__/,/;s/;.__/,/;s/;.__/,/;s/;.__/,/;s/;.__/,/;s/;.__/,/;s/|/,/;s/|/,/' data/finalcombined/${SAMPLE}.ITS1.classified | sed '1d' > data/finalcombined/${SAMPLE}.ITS1.labeled.csv
